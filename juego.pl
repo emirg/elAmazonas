5f13771a22a3811f42bdebca343b969b6c056b71
Emiliano Rios Gavagnin <emiriosgavag@gmail.com>
2019-04-01 21:18:19 -0300

    Commit inicial

---
+++ juego.pl
@@ -0,0 +1,1269 @@
+%para ejecutar ?-inicio,juego. (Run!)
+
+ 
+
+%roles o y x
+
+role(o).
+
+role(x).
+
+ 
+
+%estado inicial
+
+%Fila 1
+
+init(cell(1,1,a)).
+
+init(cell(1,2,a)).
+
+init(cell(1,3,a)).
+
+init(cell(1,4,a)).
+
+init(cell(1,5,a)).
+
+init(cell(1,6,a)).
+
+init(cell(1,7,a)).
+
+init(cell(1,8,a)).
+
+init(cell(1,9,a)).
+
+init(cell(1,10,a)).
+
+init(cell(1,11,a)).
+
+init(cell(1,12,a)).
+
+init(cell(1,13,a)).
+
+init(cell(1,14,a)).
+
+init(cell(1,15,a)).
+
+init(cell(1,16,a)).
+
+init(cell(1,17,a)).
+
+init(cell(1,18,a)).
+
+init(cell(1,19,a)).
+
+init(cell(1,20,a)).
+
+%Fila 2
+
+init(cell(2,1,a)).
+
+init(cell(2,2,a)).
+
+init(cell(2,3,a)).
+
+init(cell(2,4,a)).
+
+init(cell(2,5,a)).
+
+init(cell(2,6,a)).
+
+init(cell(2,7,a)).
+
+init(cell(2,8,a)).
+
+init(cell(2,9,a)).
+
+init(cell(2,10,a)).
+
+init(cell(2,11,a)).
+
+init(cell(2,12,a)).
+
+init(cell(2,13,a)).
+
+init(cell(2,14,a)).
+
+init(cell(2,15,a)).
+
+init(cell(2,16,a)).
+
+init(cell(2,17,a)).
+
+init(cell(2,18,a)).
+
+init(cell(2,19,a)).
+
+init(cell(2,20,a)).
+
+%Fila 3
+
+init(cell(3,1,a)).
+
+init(cell(3,2,a)).
+
+init(cell(3,3,a)).
+
+init(cell(3,4,a)).
+
+init(cell(3,5,a)).
+
+init(cell(3,6,a)).
+
+init(cell(3,7,a)).
+
+init(cell(3,8,a)).
+
+init(cell(3,9,a)).
+
+init(cell(3,10,a)).
+
+init(cell(3,11,a)).
+
+init(cell(3,12,a)).
+
+init(cell(3,13,a)).
+
+init(cell(3,14,a)).
+
+init(cell(3,15,a)).
+
+init(cell(3,16,a)).
+
+init(cell(3,17,a)).
+
+init(cell(3,18,a)).
+
+init(cell(3,19,a)).
+
+init(cell(3,20,a)).
+
+%Fila 4
+
+init(cell(4,1,a)).
+
+init(cell(4,2,a)).
+
+init(cell(4,3,a)).
+
+init(cell(4,4,a)).
+
+init(cell(4,5,a)).
+
+init(cell(4,6,a)).
+
+init(cell(4,7,a)).
+
+init(cell(4,8,a)).
+
+init(cell(4,9,a)).
+
+init(cell(4,10,a)).
+
+init(cell(4,11,a)).
+
+init(cell(4,12,a)).
+
+init(cell(4,13,a)).
+
+init(cell(4,14,a)).
+
+init(cell(4,15,a)).
+
+init(cell(4,16,a)).
+
+init(cell(4,17,a)).
+
+init(cell(4,18,a)).
+
+init(cell(4,19,a)).
+
+init(cell(4,20,a)).
+
+%Fila 5
+
+init(cell(5,1,a)).
+
+init(cell(5,2,a)).
+
+init(cell(5,3,a)).
+
+init(cell(5,4,a)).
+
+init(cell(5,5,a)).
+
+init(cell(5,6,a)).
+
+init(cell(5,7,a)).
+
+init(cell(5,8,a)).
+
+init(cell(5,9,a)).
+
+init(cell(5,10,a)).
+
+init(cell(5,11,a)).
+
+init(cell(5,12,a)).
+
+init(cell(5,13,a)).
+
+init(cell(5,14,a)).
+
+init(cell(5,15,a)).
+
+init(cell(5,16,a)).
+
+init(cell(5,17,a)).
+
+init(cell(5,18,a)).
+
+init(cell(5,19,a)).
+
+init(cell(5,20,a)).
+
+%Fila 6
+
+init(cell(6,1,a)).
+
+init(cell(6,2,a)).
+
+init(cell(6,3,a)).
+
+init(cell(6,4,a)).
+
+init(cell(6,5,a)).
+
+init(cell(6,6,a)).
+
+init(cell(6,7,a)).
+
+init(cell(6,8,a)).
+
+init(cell(6,9,a)).
+
+init(cell(6,10,a)).
+
+init(cell(6,11,a)).
+
+init(cell(6,12,a)).
+
+init(cell(6,13,a)).
+
+init(cell(6,14,a)).
+
+init(cell(6,15,a)).
+
+init(cell(6,16,a)).
+
+init(cell(6,17,a)).
+
+init(cell(6,18,a)).
+
+init(cell(6,19,a)).
+
+init(cell(6,20,a)).
+
+%Fila 7
+
+init(cell(7,1,a)).
+
+init(cell(7,2,a)).
+
+init(cell(7,3,a)).
+
+init(cell(7,4,a)).
+
+init(cell(7,5,a)).
+
+init(cell(7,6,a)).
+
+init(cell(7,7,a)).
+
+init(cell(7,8,a)).
+
+init(cell(7,9,a)).
+
+init(cell(7,10,a)).
+
+init(cell(7,11,a)).
+
+init(cell(7,12,a)).
+
+init(cell(7,13,a)).
+
+init(cell(7,14,a)).
+
+init(cell(7,15,a)).
+
+init(cell(7,16,a)).
+
+init(cell(7,17,a)).
+
+init(cell(7,18,a)).
+
+init(cell(7,19,a)).
+
+init(cell(7,20,a)).
+
+%Fila 8
+
+init(cell(8,1,a)).
+
+init(cell(8,2,a)).
+
+init(cell(8,3,a)).
+
+init(cell(8,4,a)).
+
+init(cell(8,5,a)).
+
+init(cell(8,6,a)).
+
+init(cell(8,7,a)).
+
+init(cell(8,8,a)).
+
+init(cell(8,9,a)).
+
+init(cell(8,10,a)).
+
+init(cell(8,11,a)).
+
+init(cell(8,12,a)).
+
+init(cell(8,13,a)).
+
+init(cell(8,14,a)).
+
+init(cell(8,15,a)).
+
+init(cell(8,16,a)).
+
+init(cell(8,17,a)).
+
+init(cell(8,18,a)).
+
+init(cell(8,19,a)).
+
+init(cell(8,20,a)).
+
+%Fila 9
+
+init(cell(9,1,a)).
+
+init(cell(9,2,a)).
+
+init(cell(9,3,a)).
+
+init(cell(9,4,a)).
+
+init(cell(9,5,a)).
+
+init(cell(9,6,a)).
+
+init(cell(9,7,a)).
+
+init(cell(9,8,a)).
+
+init(cell(9,9,a)).
+
+init(cell(9,10,a)).
+
+init(cell(9,11,a)).
+
+init(cell(9,12,a)).
+
+init(cell(9,13,a)).
+
+init(cell(9,14,a)).
+
+init(cell(9,15,a)).
+
+init(cell(9,16,a)).
+
+init(cell(9,17,a)).
+
+init(cell(9,18,a)).
+
+init(cell(9,19,a)).
+
+init(cell(9,20,a)).
+
+%Fila 10
+
+init(cell(10,1,a)).
+
+init(cell(10,2,a)).
+
+init(cell(10,3,a)).
+
+init(cell(10,4,a)).
+
+init(cell(10,5,a)).
+
+init(cell(10,6,a)).
+
+init(cell(10,7,a)).
+
+init(cell(10,8,a)).
+
+init(cell(10,9,a)).
+
+init(cell(10,10,a)).
+
+init(cell(10,11,a)).
+
+init(cell(10,12,a)).
+
+init(cell(10,13,a)).
+
+init(cell(10,14,a)).
+
+init(cell(10,15,a)).
+
+init(cell(10,16,a)).
+
+init(cell(10,17,a)).
+
+init(cell(10,18,a)).
+
+init(cell(10,19,a)).
+
+init(cell(10,20,a)).
+
+%%Fila 11
+
+init(cell(11,1,a)).
+
+init(cell(11,2,a)).
+
+init(cell(11,3,a)).
+
+init(cell(11,4,a)).
+
+init(cell(11,5,a)).
+
+init(cell(11,6,a)).
+
+init(cell(11,7,a)).
+
+init(cell(11,8,a)).
+
+init(cell(11,9,a)).
+
+init(cell(11,10,a)).
+
+init(cell(11,11,a)).
+
+init(cell(11,12,a)).
+
+init(cell(11,13,a)).
+
+init(cell(11,14,a)).
+
+init(cell(11,15,a)).
+
+init(cell(11,16,a)).
+
+init(cell(11,17,a)).
+
+init(cell(11,18,a)).
+
+init(cell(11,19,a)).
+
+init(cell(11,20,a)).
+
+%%Fila 12
+
+init(cell(12,1,a)).
+
+init(cell(12,2,a)).
+
+init(cell(12,3,a)).
+
+init(cell(12,4,a)).
+
+init(cell(12,5,a)).
+
+init(cell(12,6,a)).
+
+init(cell(12,7,a)).
+
+init(cell(12,8,a)).
+
+init(cell(12,9,a)).
+
+init(cell(12,10,a)).
+
+init(cell(12,11,a)).
+
+init(cell(12,12,a)).
+
+init(cell(12,13,a)).
+
+init(cell(12,14,a)).
+
+init(cell(12,15,a)).
+
+init(cell(12,16,a)).
+
+init(cell(12,17,a)).
+
+init(cell(12,18,a)).
+
+init(cell(12,19,a)).
+
+init(cell(12,20,a)).
+
+%%Fila 13
+
+init(cell(13,1,a)).
+
+init(cell(13,2,a)).
+
+init(cell(13,3,a)).
+
+init(cell(13,4,a)).
+
+init(cell(13,5,a)).
+
+init(cell(13,6,a)).
+
+init(cell(13,7,a)).
+
+init(cell(13,8,a)).
+
+init(cell(13,9,a)).
+
+init(cell(13,10,a)).
+
+init(cell(13,11,a)).
+
+init(cell(13,12,a)).
+
+init(cell(13,13,a)).
+
+init(cell(13,14,a)).
+
+init(cell(13,15,a)).
+
+init(cell(13,16,a)).
+
+init(cell(13,17,a)).
+
+init(cell(13,18,a)).
+
+init(cell(13,19,a)).
+
+init(cell(13,20,a)).
+
+%%Fila 14
+
+init(cell(14,1,a)).
+
+init(cell(14,2,a)).
+
+init(cell(14,3,a)).
+
+init(cell(14,4,a)).
+
+init(cell(14,5,a)).
+
+init(cell(14,6,a)).
+
+init(cell(14,7,a)).
+
+init(cell(14,8,a)).
+
+init(cell(14,9,a)).
+
+init(cell(14,10,a)).
+
+init(cell(14,11,a)).
+
+init(cell(14,12,a)).
+
+init(cell(14,13,a)).
+
+init(cell(14,14,a)).
+
+init(cell(14,15,a)).
+
+init(cell(14,16,a)).
+
+init(cell(14,17,a)).
+
+init(cell(14,18,a)).
+
+init(cell(14,19,a)).
+
+init(cell(14,20,a)).
+
+%%Fila 15
+
+init(cell(15,1,a)).
+
+init(cell(15,2,a)).
+
+init(cell(15,3,a)).
+
+init(cell(15,4,a)).
+
+init(cell(15,5,a)).
+
+init(cell(15,6,a)).
+
+init(cell(15,7,a)).
+
+init(cell(15,8,a)).
+
+init(cell(15,9,a)).
+
+init(cell(15,10,a)).
+
+init(cell(15,11,a)).
+
+init(cell(15,12,a)).
+
+init(cell(15,13,a)).
+
+init(cell(15,14,a)).
+
+init(cell(15,15,a)).
+
+init(cell(15,16,a)).
+
+init(cell(15,17,a)).
+
+init(cell(15,18,a)).
+
+init(cell(15,19,a)).
+
+init(cell(15,20,a)).
+
+%%Fila 16
+
+init(cell(16,1,a)).
+
+init(cell(16,2,a)).
+
+init(cell(16,3,a)).
+
+init(cell(16,4,a)).
+
+init(cell(16,5,a)).
+
+init(cell(16,6,a)).
+
+init(cell(16,7,a)).
+
+init(cell(16,8,a)).
+
+init(cell(16,9,a)).
+
+init(cell(16,10,a)).
+
+init(cell(16,11,a)).
+
+init(cell(16,12,a)).
+
+init(cell(16,13,a)).
+
+init(cell(16,14,a)).
+
+init(cell(16,15,a)).
+
+init(cell(16,16,a)).
+
+init(cell(16,17,a)).
+
+init(cell(16,18,a)).
+
+init(cell(16,19,a)).
+
+init(cell(16,20,a)).
+
+%%Fila 17
+
+init(cell(17,1,a)).
+
+init(cell(17,2,a)).
+
+init(cell(17,3,a)).
+
+init(cell(17,4,a)).
+
+init(cell(17,5,a)).
+
+init(cell(17,6,a)).
+
+init(cell(17,7,a)).
+
+init(cell(17,8,a)).
+
+init(cell(17,9,a)).
+
+init(cell(17,10,a)).
+
+init(cell(17,11,a)).
+
+init(cell(17,12,a)).
+
+init(cell(17,13,a)).
+
+init(cell(17,14,a)).
+
+init(cell(17,15,a)).
+
+init(cell(17,16,a)).
+
+init(cell(17,17,a)).
+
+init(cell(17,18,a)).
+
+init(cell(17,19,a)).
+
+init(cell(17,20,a)).
+
+%%Fila 18
+
+init(cell(18,1,a)).
+
+init(cell(18,2,a)).
+
+init(cell(18,3,a)).
+
+init(cell(18,4,a)).
+
+init(cell(18,5,a)).
+
+init(cell(18,6,a)).
+
+init(cell(18,7,a)).
+
+init(cell(18,8,a)).
+
+init(cell(18,9,a)).
+
+init(cell(18,10,a)).
+
+init(cell(18,11,a)).
+
+init(cell(18,12,a)).
+
+init(cell(18,13,a)).
+
+init(cell(18,14,a)).
+
+init(cell(18,15,a)).
+
+init(cell(18,16,a)).
+
+init(cell(18,17,a)).
+
+init(cell(18,18,a)).
+
+init(cell(18,19,a)).
+
+init(cell(18,20,a)).
+
+%%Fila 19
+
+init(cell(19,1,a)).
+
+init(cell(19,2,a)).
+
+init(cell(19,3,a)).
+
+init(cell(19,4,a)).
+
+init(cell(19,5,a)).
+
+init(cell(19,6,a)).
+
+init(cell(19,7,a)).
+
+init(cell(19,8,a)).
+
+init(cell(19,9,a)).
+
+init(cell(19,10,a)).
+
+init(cell(19,11,a)).
+
+init(cell(19,12,a)).
+
+init(cell(19,13,a)).
+
+init(cell(19,14,a)).
+
+init(cell(19,15,a)).
+
+init(cell(19,16,a)).
+
+init(cell(19,17,a)).
+
+init(cell(19,18,a)).
+
+init(cell(19,19,a)).
+
+init(cell(19,20,a)).
+
+%%Fila 20
+
+init(cell(20,1,a)).
+
+init(cell(20,2,a)).
+
+init(cell(20,3,a)).
+
+init(cell(20,4,a)).
+
+init(cell(20,5,a)).
+
+init(cell(20,6,a)).
+
+init(cell(20,7,a)).
+
+init(cell(20,8,a)).
+
+init(cell(20,9,a)).
+
+init(cell(20,10,a)).
+
+init(cell(20,11,a)).
+
+init(cell(20,12,a)).
+
+init(cell(20,13,a)).
+
+init(cell(20,14,a)).
+
+init(cell(20,15,a)).
+
+init(cell(20,16,a)).
+
+init(cell(20,17,a)).
+
+init(cell(20,18,a)).
+
+init(cell(20,19,a)).
+
+init(cell(20,20,a)).
+
+ 
+
+ 
+
+init(control(o)).
+
+ 
+
+%posibles valores que pueden tener las relaciones
+
+base(control(X)):- role(X).
+
+base(cell(X,Y,a)):- index(X),index(Y).
+
+base(cell(X,Y,R)):- role(R),index(X),index(Y).
+
+ 
+
+index(1).
+
+index(2).
+
+index(3).
+
+ 
+
+%posibles valores que pueden tener las entradas
+
+input(R,mark(X,Y)):- role(R),index(X),index(Y).
+
+input(R,noop):-role(R).
+
+ 
+
+%movimientos legales
+
+legal(W,mark(X,Y)) :-
+
+                t(cell(X,Y,a)),t(control(W)).
+
+legal(o,noop) :-
+
+                t(control(x)).
+
+legal(x,noop) :-
+
+      t(control(o)).
+
+ 
+
+%próximo estado
+
+next(cell(M,N,x)) :-
+
+      does(x,mark(M,N)),
+
+      t(cell(M,N,a)).
+
+ 
+
+next(cell(M,N,o)) :-
+
+      does(o,mark(M,N)),
+
+      t(cell(M,N,a)).
+
+ 
+
+next(cell(M,N,W)) :-
+
+      t(cell(M,N,W)),
+
+      distinct(W,a).
+
+ 
+
+next(cell(M,N,a)) :-
+
+      does(_W,mark(J,_K)),
+
+      t(cell(M,N,a)),
+
+      distinct(M,J).
+
+ 
+
+next(cell(M,N,a)) :-
+
+      does(_W,mark(_J,K)),
+
+      t(cell(M,N,a)),
+
+      distinct(N,K).
+
+ 
+
+next(control(o)) :-
+
+      t(control(x)).
+
+ 
+
+next(control(x)) :-
+
+      t(control(o)).
+
+ 
+
+%Cambiamos las reglas pierde el que hace TATETI
+
+%goal(x,100) :- line(x),\+line(o).
+
+%goal(x,50) :-  \+line(x), \+line(o).
+
+%goal(x,0) :- \+line(x), line(o).
+
+ 
+
+%goal(o,100) :- \+line(x), line(o).
+
+%goal(o,50) :- \+line(x), \+line(o).
+
+%goal(o,0) :- line(x), \+line(o).
+
+ 
+
+goal(x,0) :- line(x),\+line(o).
+
+goal(x,50) :-  \+line(x), \+line(o).
+
+goal(x,100) :- \+line(x), line(o).
+
+ 
+
+goal(o,0) :- \+line(x), line(o).
+
+goal(o,50) :- \+line(x), \+line(o).
+
+goal(o,100) :- line(x), \+line(o).
+
+ 
+
+ 
+
+line(Z) :- row(_M,Z).
+
+    line(Z) :- column(_M,Z).
+
+    line(Z) :- diagonal(Z).
+
+ 
+
+    row(M,Z) :-
+
+      t(cell(M,1,Z)) ,
+
+      t(cell(M,2,Z)) ,
+
+      t(cell(M,3,Z)).
+
+ 
+
+    column(_M,Z) :-
+
+      t(cell(1,N,Z)) ,
+
+      t(cell(2,N,Z)) ,
+
+      t(cell(3,N,Z)).
+
+ 
+
+    diagonal(Z) :-
+
+      t(cell(1,1,Z)) ,
+
+      t(cell(2,2,Z)) ,
+
+      t(cell(3,3,Z)).
+
+ 
+
+    diagonal(Z) :-
+
+      t(cell(1,3,Z)) ,
+
+      t(cell(2,2,Z)) ,
+
+      t(cell(3,1,Z)).
+
+ 
+
+terminal :- line(x).
+
+terminal :- line(o).
+
+terminal :- \+open.
+
+ 
+
+open :- t(cell(X,Y,a)),
+
+index(X),
+
+index(Y).
+
+ 
+
+ 
+
+ 
+
+distinct(X,Y):-
+
+X\==Y.
+
+ 
+
+%inicializa estado inicial y borra historial
+
+:-dynamic t/1,h/2,estado/1,does/2.
+
+inicio:-
+
+                retractall(t(_)),
+
+                retractall(h(_,_)),
+
+                retractall(estado(_)),
+
+                crea_estado_inicial.
+
+ 
+
+%crea estado inicial
+
+crea_estado_inicial:-
+
+                init(X),
+
+    \+(t(X)),
+
+                assert(t(X)),
+
+    assert(h(0,X)),
+
+                crea_estado_inicial.
+
+crea_estado_inicial:-
+
+                assert(estado(1)).
+
+ 
+
+ 
+
+% gestor del juego
+
+% borra acciones viejas
+
+% busca nuevas acciones
+
+%mark calcula próximo estado
+
+% crea proximo estado
+
+ 
+
+juego:- \+terminal,
+
+retractall(does(_X,_A)),
+
+inserta_acciones,
+
+proximo_estado,
+
+retractall(t(_Y)),
+
+crea_estado,
+
+imprime,
+
+juego.
+
+ 
+
+juego:- terminal,
+
+                goal(x,Px),goal(o,Po),
+
+                display('x gano '),display(Px),display(' puntos y o gano '),display(Po),display(' puntos.').
+
+ 
+
+% busca las nuevas acciones de los jugadores y las inserta
+
+inserta_acciones:- t(control(X)),
+
+   jugador(X,A), legal(X,A),
+
+   assert(does(X,A)),
+
+   role(O), distinct(X,O),
+
+   assert(does(O,noop)).
+
+ 
+
+%calcula el próximo estado
+
+proximo_estado:-
+
+        estado(E),
+
+                next(Y),
+
+                \+(h(E,Y)),
+
+                assert(h(E,Y)),
+
+                proximo_estado.
+
+proximo_estado.
+
+ 
+
+%crea el estado actual
+
+crea_estado:-
+
+                estado(E),
+
+                h(E,Y),
+
+                \+(t(Y)),
+
+                assert(t(Y)),
+
+                crea_estado.
+
+crea_estado:-
+
+retract(estado(N)),
+
+N2 is N +1,
+
+assert(estado(N2)).
+
+imprime_fila(N):-
+
+                t(cell(N,1,C1)),t(cell(N,2,C2)),t(cell(N,3,C3)),
+
+    t(cell(N,4,C4)),t(cell(N,5,C5)),t(cell(N,6,C6)),
+
+    t(cell(N,7,C7)),t(cell(N,8,C8)),t(cell(N,9,C9)),
+
+    t(cell(N,10,C10)),t(cell(N,11,C11)),t(cell(N,12,C12)),
+
+    t(cell(N,13,C13)),t(cell(N,14,C14)),t(cell(N,15,C15)),
+
+    t(cell(N,16,C16)),t(cell(N,17,C17)),t(cell(N,18,C18)),
+
+    t(cell(N,19,C19)),t(cell(N,20,C20)),
+
+                display(C1),display(C2),display(C3),
+
+    display(C4),display(C5),display(C6),
+
+    display(C7),display(C8),display(C9),
+
+    display(C10),display(C11),display(C12),
+
+    display(C13),display(C14),display(C15),
+
+    display(C16),display(C17),display(C18),
+
+    display(C19),display(C20),nl.
+
+ 
+
+ 
+
+%desarrollo jugador o      
+
+jugador(o,A):-
+
+legal(o,A).
+
+ 
+
+ 
+
+%desarrollo jugador x
+
+jugador(x,X):-
+
+%legal(x,X).
+
+display('Ingrese próximo movimiento:'),
+
+read(X).
+
+% display('Ingrese Marca en Y:'),
+
+% read_term(Y,[]).
+
+%
+
+/** <examples>
+
+?- inicio,juego.
+
+*/
+
+%
