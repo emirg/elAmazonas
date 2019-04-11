0f6ad7df37520e7e8d7c07b6a0d16862dc6d6ad4
Emiliano <emiriosgavag@gmail.com>
2019-04-11 14:33:49 -0300

    Implementación mapas
    Ahora se puede importar mapas

--- juego.pl
+++ juego.pl
@@ -110,408 +110,7 @@ cant_casillas_total(N):- limites_tablero(X,Y), N is X*Y.
 %   s -> Serpiente 1
 %   c-> Serpiente 2
 
-init(cell(1,1,a)).
-init(cell(1,2,a)).
-init(cell(1,3,a)).
-init(cell(1,4,a)).
-init(cell(1,5,a)).
-init(cell(1,6,a)).
-init(cell(1,7,a)).
-init(cell(1,8,a)).
-init(cell(1,9,a)).
-init(cell(1,10,a)).
-init(cell(1,11,a)).
-init(cell(1,12,a)).
-init(cell(1,13,a)).
-init(cell(1,14,a)).
-init(cell(1,15,a)).
-init(cell(1,16,a)).
-init(cell(1,17,a)).
-init(cell(1,18,a)).
-init(cell(1,19,a)).
-init(cell(1,20,a)).
-init(cell(2,1,a)).
-init(cell(2,2,a)).
-init(cell(2,3,a)).
-init(cell(2,4,a)).
-init(cell(2,5,a)).
-init(cell(2,6,a)).
-init(cell(2,7,a)).
-init(cell(2,8,a)).
-init(cell(2,9,a)).
-init(cell(2,10,a)).
-init(cell(2,11,a)).
-init(cell(2,12,a)).
-init(cell(2,13,a)).
-init(cell(2,14,a)).
-init(cell(2,15,a)).
-init(cell(2,16,a)).
-init(cell(2,17,c)).
-init(cell(2,18,c)).
-init(cell(2,19,c)).
-init(cell(2,20,a)).
-init(cell(3,1,a)).
-init(cell(3,2,a)).
-init(cell(3,3,a)).
-init(cell(3,4,a)).
-init(cell(3,5,a)).
-init(cell(3,6,a)).
-init(cell(3,7,a)).
-init(cell(3,8,a)).
-init(cell(3,9,a)).
-init(cell(3,10,a)).
-init(cell(3,11,a)).
-init(cell(3,12,a)).
-init(cell(3,13,a)).
-init(cell(3,14,a)).
-init(cell(3,15,a)).
-init(cell(3,16,a)).
-init(cell(3,17,p)).
-init(cell(3,18,a)).
-init(cell(3,19,a)).
-init(cell(3,20,a)).
-init(cell(4,1,a)).
-init(cell(4,2,a)).
-init(cell(4,3,a)).
-init(cell(4,4,a)).
-init(cell(4,5,b)).
-init(cell(4,6,a)).
-init(cell(4,7,a)).
-init(cell(4,8,a)).
-init(cell(4,9,a)).
-init(cell(4,10,a)).
-init(cell(4,11,a)).
-init(cell(4,12,a)).
-init(cell(4,13,a)).
-init(cell(4,14,a)).
-init(cell(4,15,a)).
-init(cell(4,16,a)).
-init(cell(4,17,a)).
-init(cell(4,18,a)).
-init(cell(4,19,a)).
-init(cell(4,20,a)).
-init(cell(5,1,a)).
-init(cell(5,2,a)).
-init(cell(5,3,a)).
-init(cell(5,4,a)).
-init(cell(5,5,a)).
-init(cell(5,6,a)).
-init(cell(5,7,a)).
-init(cell(5,8,a)).
-init(cell(5,9,a)).
-init(cell(5,10,a)).
-init(cell(5,11,a)).
-init(cell(5,12,a)).
-init(cell(5,13,a)).
-init(cell(5,14,i)).
-init(cell(5,15,i)).
-init(cell(5,16,i)).
-init(cell(5,17,i)).
-init(cell(5,18,a)).
-init(cell(5,19,a)).
-init(cell(5,20,a)).
-init(cell(6,1,a)).
-init(cell(6,2,a)).
-init(cell(6,3,a)).
-init(cell(6,4,a)).
-init(cell(6,5,a)).
-init(cell(6,6,a)).
-init(cell(6,7,a)).
-init(cell(6,8,a)).
-init(cell(6,9,a)).
-init(cell(6,10,a)).
-init(cell(6,11,a)).
-init(cell(6,12,a)).
-init(cell(6,13,a)).
-init(cell(6,14,i)).
-init(cell(6,15,i)).
-init(cell(6,16,i)).
-init(cell(6,17,i)).
-init(cell(6,18,a)).
-init(cell(6,19,a)).
-init(cell(6,20,a)).
-init(cell(7,1,a)).
-init(cell(7,2,a)).
-init(cell(7,3,a)).
-init(cell(7,4,i)).
-init(cell(7,5,i)).
-init(cell(7,6,i)).
-init(cell(7,7,i)).
-init(cell(7,8,a)).
-init(cell(7,9,a)).
-init(cell(7,10,a)).
-init(cell(7,11,a)).
-init(cell(7,12,a)).
-init(cell(7,13,a)).
-init(cell(7,14,i)).
-init(cell(7,15,i)).
-init(cell(7,16,i)).
-init(cell(7,17,i)).
-init(cell(7,18,a)).
-init(cell(7,19,a)).
-init(cell(7,20,a)).
-init(cell(8,1,a)).
-init(cell(8,2,a)).
-init(cell(8,3,a)).
-init(cell(8,4,i)).
-init(cell(8,5,i)).
-init(cell(8,6,i)).
-init(cell(8,7,i)).
-init(cell(8,8,a)).
-init(cell(8,9,a)).
-init(cell(8,10,a)).
-init(cell(8,11,a)).
-init(cell(8,12,a)).
-init(cell(8,13,a)).
-init(cell(8,14,a)).
-init(cell(8,15,a)).
-init(cell(8,16,a)).
-init(cell(8,17,a)).
-init(cell(8,18,a)).
-init(cell(8,19,a)).
-init(cell(8,20,a)).
-init(cell(9,1,a)).
-init(cell(9,2,a)).
-init(cell(9,3,a)).
-init(cell(9,4,i)).
-init(cell(9,5,i)).
-init(cell(9,6,i)).
-init(cell(9,7,i)).
-init(cell(9,8,a)).
-init(cell(9,9,a)).
-init(cell(9,10,b)).
-init(cell(9,11,a)).
-init(cell(9,12,a)).
-init(cell(9,13,a)).
-init(cell(9,14,a)).
-init(cell(9,15,a)).
-init(cell(9,16,a)).
-init(cell(9,17,a)).
-init(cell(9,18,a)).
-init(cell(9,19,a)).
-init(cell(9,20,a)).
-init(cell(10,1,a)).
-init(cell(10,2,a)).
-init(cell(10,3,a)).
-init(cell(10,4,a)).
-init(cell(10,5,a)).
-init(cell(10,6,a)).
-init(cell(10,7,a)).
-init(cell(10,8,a)).
-init(cell(10,9,a)).
-init(cell(10,10,a)).
-init(cell(10,11,a)).
-init(cell(10,12,a)).
-init(cell(10,13,a)).
-init(cell(10,14,a)).
-init(cell(10,15,a)).
-init(cell(10,16,a)).
-init(cell(10,17,a)).
-init(cell(10,18,a)).
-init(cell(10,19,a)).
-init(cell(10,20,a)).
-init(cell(11,1,a)).
-init(cell(11,2,a)).
-init(cell(11,3,a)).
-init(cell(11,4,a)).
-init(cell(11,5,a)).
-init(cell(11,6,a)).
-init(cell(11,7,a)).
-init(cell(11,8,a)).
-init(cell(11,9,a)).
-init(cell(11,10,a)).
-init(cell(11,11,a)).
-init(cell(11,12,a)).
-init(cell(11,13,a)).
-init(cell(11,14,a)).
-init(cell(11,15,a)).
-init(cell(11,16,a)).
-init(cell(11,17,a)).
-init(cell(11,18,a)).
-init(cell(11,19,a)).
-init(cell(11,20,a)).
-init(cell(12,1,a)).
-init(cell(12,2,a)).
-init(cell(12,3,a)).
-init(cell(12,4,a)).
-init(cell(12,5,a)).
-init(cell(12,6,a)).
-init(cell(12,7,a)).
-init(cell(12,8,a)).
-init(cell(12,9,a)).
-init(cell(12,10,a)).
-init(cell(12,11,a)).
-init(cell(12,12,a)).
-init(cell(12,13,a)).
-init(cell(12,14,a)).
-init(cell(12,15,a)).
-init(cell(12,16,a)).
-init(cell(12,17,a)).
-init(cell(12,18,a)).
-init(cell(12,19,a)).
-init(cell(12,20,a)).
-init(cell(13,1,a)).
-init(cell(13,2,a)).
-init(cell(13,3,a)).
-init(cell(13,4,a)).
-init(cell(13,5,p)).
-init(cell(13,6,a)).
-init(cell(13,7,a)).
-init(cell(13,8,a)).
-init(cell(13,9,a)).
-init(cell(13,10,a)).
-init(cell(13,11,a)).
-init(cell(13,12,a)).
-init(cell(13,13,a)).
-init(cell(13,14,a)).
-init(cell(13,15,a)).
-init(cell(13,16,a)).
-init(cell(13,17,a)).
-init(cell(13,18,a)).
-init(cell(13,19,a)).
-init(cell(13,20,a)).
-init(cell(14,1,a)).
-init(cell(14,2,a)).
-init(cell(14,3,a)).
-init(cell(14,4,a)).
-init(cell(14,5,a)).
-init(cell(14,6,a)).
-init(cell(14,7,a)).
-init(cell(14,8,a)).
-init(cell(14,9,a)).
-init(cell(14,10,a)).
-init(cell(14,11,a)).
-init(cell(14,12,a)).
-init(cell(14,13,a)).
-init(cell(14,14,a)).
-init(cell(14,15,a)).
-init(cell(14,16,b)).
-init(cell(14,17,a)).
-init(cell(14,18,a)).
-init(cell(14,19,a)).
-init(cell(14,20,a)).
-init(cell(15,1,a)).
-init(cell(15,2,a)).
-init(cell(15,3,a)).
-init(cell(15,4,a)).
-init(cell(15,5,a)).
-init(cell(15,6,a)).
-init(cell(15,7,a)).
-init(cell(15,8,a)).
-init(cell(15,9,a)).
-init(cell(15,10,a)).
-init(cell(15,11,a)).
-init(cell(15,12,a)).
-init(cell(15,13,a)).
-init(cell(15,14,a)).
-init(cell(15,15,a)).
-init(cell(15,16,a)).
-init(cell(15,17,a)).
-init(cell(15,18,a)).
-init(cell(15,19,a)).
-init(cell(15,20,a)).
-init(cell(16,1,a)).
-init(cell(16,2,a)).
-init(cell(16,3,a)).
-init(cell(16,4,a)).
-init(cell(16,5,a)).
-init(cell(16,6,a)).
-init(cell(16,7,a)).
-init(cell(16,8,a)).
-init(cell(16,9,a)).
-init(cell(16,10,a)).
-init(cell(16,11,a)).
-init(cell(16,12,a)).
-init(cell(16,13,a)).
-init(cell(16,14,a)).
-init(cell(16,15,a)).
-init(cell(16,16,a)).
-init(cell(16,17,a)).
-init(cell(16,18,a)).
-init(cell(16,19,a)).
-init(cell(16,20,a)).
-init(cell(17,1,a)).
-init(cell(17,2,a)).
-init(cell(17,3,a)).
-init(cell(17,4,a)).
-init(cell(17,5,a)).
-init(cell(17,6,a)).
-init(cell(17,7,a)).
-init(cell(17,8,a)).
-init(cell(17,9,a)).
-init(cell(17,10,i)).
-init(cell(17,11,i)).
-init(cell(17,12,i)).
-init(cell(17,13,i)).
-init(cell(17,14,a)).
-init(cell(17,15,a)).
-init(cell(17,16,a)).
-init(cell(17,17,s)).
-init(cell(17,18,s)).
-init(cell(17,19,s)).
-init(cell(17,20,a)).
-init(cell(18,1,a)).
-init(cell(18,2,a)).
-init(cell(18,3,a)).
-init(cell(18,4,b)).
-init(cell(18,5,a)).
-init(cell(18,6,a)).
-init(cell(18,7,a)).
-init(cell(18,8,a)).
-init(cell(18,9,a)).
-init(cell(18,10,i)).
-init(cell(18,11,i)).
-init(cell(18,12,i)).
-init(cell(18,13,i)).
-init(cell(18,14,a)).
-init(cell(18,15,a)).
-init(cell(18,16,a)).
-init(cell(18,17,p)).
-init(cell(18,18,a)).
-init(cell(18,19,a)).
-init(cell(18,20,a)).
-init(cell(19,1,a)).
-init(cell(19,2,a)).
-init(cell(19,3,a)).
-init(cell(19,4,a)).
-init(cell(19,5,a)).
-init(cell(19,6,a)).
-init(cell(19,7,a)).
-init(cell(19,8,a)).
-init(cell(19,9,a)).
-init(cell(19,10,i)).
-init(cell(19,11,i)).
-init(cell(19,12,i)).
-init(cell(19,13,i)).
-init(cell(19,14,a)).
-init(cell(19,15,a)).
-init(cell(19,16,a)).
-init(cell(19,17,a)).
-init(cell(19,18,a)).
-init(cell(19,19,a)).
-init(cell(19,20,a)).
-init(cell(20,1,a)).
-init(cell(20,2,a)).
-init(cell(20,3,a)).
-init(cell(20,4,a)).
-init(cell(20,5,a)).
-init(cell(20,6,a)).
-init(cell(20,7,a)).
-init(cell(20,8,a)).
-init(cell(20,9,a)).
-init(cell(20,10,a)).
-init(cell(20,11,a)).
-init(cell(20,12,a)).
-init(cell(20,13,a)).
-init(cell(20,14,a)).
-init(cell(20,15,a)).
-init(cell(20,16,a)).
-init(cell(20,17,a)).
-init(cell(20,18,a)).
-init(cell(20,19,a)).
-init(cell(20,20,a)).
-init(serpiente(c,left,[(2,17),(2,18),(2,19)],3)). % Charlie - serpiente/4: jugadorDueño,orientacion,[cabeza|cuerpo],vidas
-init(serpiente(s,left,[(17,17),(17,18),(17,19)],3)). % Simon
+:- include('mapa2.pl').
 init(control(c)). %Empieza jugando Charlie
 
 
@@ -524,16 +123,17 @@ base(cell(X,Y,p)):- index(X),index(Y).
 base(cell(X,Y,R)):- role(R),index(X),index(Y).
 base(serpiente(R,D,L,V)):- role(R), direccion(D),is_list(L),V>0.
 
-index( 1 ).
-index( 2 ).
-index( 3 ).
-index( 4 ).
-index( 5 ).
-index( 6 ).
-index( 7 ).
-index( 8 ).
-index( 9 ).
-index( 10 ).
+index(1).
+index(2).
+index(3).
+index(4).
+index(5).
+index(6).
+index(7).
+index(8).
+index(9).
+index(10).
+/*
 index( 11 ).
 index( 12 ).
 index( 13 ).
@@ -544,7 +144,7 @@ index( 17 ).
 index( 18 ).
 index( 19 ).
 index( 20 ).
-
+*/
 %%%%%%%%%%
 % Inputs %
 %%%%%%%%%%
@@ -630,8 +230,7 @@ next(cell(M,N,R)) :-
   does(R,move(_)),
   t(serpiente(R,_,S,V)),
   V > 0,
-  \+last(S,(M,N)),
-  \+head(S,(M,N)).
+  \+last(S,(M,N)).
 
 % Celda -> Mantiene -> Serpiente -> Cola -> Comio persona
 next(cell(M,N,R)) :-
@@ -639,7 +238,7 @@ next(cell(M,N,R)) :-
   does(R,move(D)),
   t(serpiente(R,_,[(X,Y)|Sv],V)),
   V > 0,
-  last(S,(M,N)),  
+  last([(X,Y)|Sv],(M,N)),  
   decrease(D,A,B),
   XCabezaNueva is X + A,
   YCabezaNueva is Y + B,
@@ -649,30 +248,30 @@ next(cell(M,N,R)) :-
 % Celda -> Cambio -> Serpiente -> Cabeza -> Movio a agua
 next(cell(M,N,R)) :- 
   t(cell(M,N,a)),
-  does(R,move(O)),
+  does(R,move(D)),
   t(serpiente(R,_,[(X,Y)|_],V)),
   V > 0,
-  decrease(O,A,B),
+  decrease(D,A,B),
   M is X+A,
   N is Y+B.
 
 % Celda -> Cambio -> Serpiente -> Cabeza -> Comio persona
 next(cell(M,N,R)) :-
   t(cell(M,N,p)),
-  does(R,move(O)),
+  does(R,move(D)),
   t(serpiente(R,_,[(X,Y)|_],V)),
   V > 0,
-  decrease(O,A,B),
+  decrease(D,A,B),
   M is X+A,
   N is Y+B.
 
 % Celda -> Cambio -> Serpiente -> Cabeza -> Comio barril
-next(cell(M,N,R)) :-
+next(cell(M,N,R)) :-  
   t(cell(M,N,b)),
-  does(R,move(O)),
+  does(R,move(D)),
   t(serpiente(R,_,[(X,Y)|_],V)),
   V > 0,
-  decrease(O,A,B),
+  decrease(D,A,B),
   M is X+A,
   N is Y+B.
 
@@ -682,12 +281,12 @@ next(cell(M,N,a)) :-
   does(R,move(D)),
   t(serpiente(R,_,[(X,Y)|Sv],V)),
   V > 0,
-  last(S,(M,N)),  
+  last([(X,Y)|Sv],(M,N)),  
   decrease(D,A,B),
-  XCabezaNueva is X + A,
-  YCabezaNueva is Y + B,
-  t(cell(XCabezaNueva,YCabezaNueva,A)),
-  distinct(A,p).
+  XCabezaNueva is X+A,
+  YCabezaNueva is Y+B,
+  t(cell(XCabezaNueva,YCabezaNueva,Z)),
+  distinct(Z,p).
 
 % Celda -> Mantiene -> SerpienteEnemiga 
 next(cell(M,N,J2)):-
@@ -746,6 +345,7 @@ next(serpiente(R,D,S,0)):-
   XCabezaNueva is X + A,
   YCabezaNueva is Y + B,
   t(cell(XCabezaNueva,YCabezaNueva,R2)),
+  role(R2),
   distinct(R,R2).
 
 % Serpiente -> Movio -> Persona -> Cabeza/Cuerpo/Cola
@@ -760,17 +360,11 @@ next(serpiente(R,D,S,V)):-
   t(cell(XCabezaNueva,YCabezaNueva,p)), 
   addBegin((XCabezaNueva,YCabezaNueva),L1,S).
 
-% Serpiente -> No se movio -> SerpienteEnemiga -> Vida
-next(serpiente(R2,D,S,0)):-
-  t(control(R)),
-  does(R,move(D)),
-  t(serpiente(R,_,[(X,Y)|Sv],V)),
-  V > 0,
-  decrease(D,A,B),
-  XCabezaNueva is X + A,
-  YCabezaNueva is Y + B,
-  t(cell(XCabezaNueva,YCabezaNueva,R2)),
-  distinct(R,R2).
+next(serpiente(R,D,S,V)):-
+  t(control(R2)),
+  t(serpiente(R,D,S,V)),
+  does(R,noop),
+  distinct(R2,R).
 
 
 
@@ -813,7 +407,7 @@ distinct(X,Y):- X\==Y.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% GESTOR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test:-
   inicio,
-  assert(does(c,move(left))),
+  assert(does(c,move(right))),
   assert(does(s,noop)).
 
 
@@ -921,7 +515,7 @@ imprime:-
   imprime_fila(7),
   imprime_fila(8),
   imprime_fila(9),
-  imprime_fila(10),
+  imprime_fila(10),/*
   imprime_fila(11),
   imprime_fila(12),
   imprime_fila(13),
@@ -931,7 +525,7 @@ imprime:-
   imprime_fila(17),
   imprime_fila(18),
   imprime_fila(19),
-  imprime_fila(20),
+  imprime_fila(20),*/
   display('********').
 
 imprime_fila(N):-
@@ -942,13 +536,13 @@ imprime_fila(N):-
 
     t(cell(N,7,C7)),t(cell(N,8,C8)),t(cell(N,9,C9)),
 
-    t(cell(N,10,C10)),t(cell(N,11,C11)),t(cell(N,12,C12)),
+    t(cell(N,10,C10)),/*t(cell(N,11,C11)),t(cell(N,12,C12)),
 
     t(cell(N,13,C13)),t(cell(N,14,C14)),t(cell(N,15,C15)),
 
     t(cell(N,16,C16)),t(cell(N,17,C17)),t(cell(N,18,C18)),
 
-    t(cell(N,19,C19)),t(cell(N,20,C20)),
+    t(cell(N,19,C19)),t(cell(N,20,C20)),*/
 
     display(C1),display(C2),display(C3),
 
@@ -956,13 +550,13 @@ imprime_fila(N):-
 
     display(C7),display(C8),display(C9),
 
-    display(C10),display(C11),display(C12),
+    display(C10),/*display(C11),display(C12),
 
     display(C13),display(C14),display(C15),
 
     display(C16),display(C17),display(C18),
 
-    display(C19),display(C20),nl.
+    display(C19),display(C20),*/nl.
 
  
 
