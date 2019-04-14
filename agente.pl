6ebc25970c39c2f84d12541712fe0262c570d4ae
german kiessling <gkiessling_95@hotmail.com>
2019-04-14 13:19:44 -0300

    cambie algunos casos de comparación
    igual puede que funcione con los anteriores , pero le agregue para que no choque con otra serpiente

--- agente.pl
+++ agente.pl
@@ -1,11 +1,12 @@
-agente2(Rol,MejorAccion):-  
-  getPersonaCercana((X,Y),D),
+agente2(Rol,MejorAccion):- 
+  t(control(Rol)), 
+  getPersonaCercana((X,Y),_D),
   getAccionesLegales([],AccionesLegales),
-  %write('Acciones validas: '),write(AccionesLegales),nl,
+  write('Acciones validas: '),write(AccionesLegales),nl,
   getVecinosValidos((X,Y),[],VecinosValidos),
-  %write('Vecinos validos: '),write(VecinosValidos),nl,
-  getMejorAccion(VecinosValidos,_,200,MejorAccion,_).
-  %write('Mejor accion: '),write(MejorAccion),nl.
+  write('Vecinos validos: '),write(VecinosValidos),nl,
+  getMejorAccion(VecinosValidos,_,200,MejorAccion,_),
+  write('Mejor accion: '),write(MejorAccion),nl.
   
 
 
@@ -24,48 +25,61 @@ distancia((X1,Y1),(X2,Y2),D) :-
 
 
 getVecinosValidos((XP,YP),L0,L1):-
+  %write('Xpersona  '),write(XP),nl,
+  %write('Ypersona  '),write(YP),nl,
   t(control(Rol)),
   t(serpiente(Rol,D,[(X,Y)|_],_)),
   allowed(D,M),
+  %write('allowed D '),write(D),nl,
+  %write('allowed M '),write(M),nl,
   decrease(M,A,B),
   X1 is X+A,
-  Y1 is Y+B,
+  Y1 is Y+B,nl,
+  %write('XValido  '),write(X1),nl,
+  %write('YValido  '),write(Y1),nl,
   t(cell(X1,Y1,C)),
-  distinct(C,b),
-  distinct(C,i),
+  %write('cell con C  '),write(C),nl,
+  (\+distinct(C,a);\+distinct(C,p)),
+  %distinct(C,b),
+  %distinct(C,i),
+  role(Rol1),
+  distinct(C,Rol1),
+  %write(C),
   \+in((X1,Y1,M,_),L0),
   distancia((X1,Y1),(XP,YP),Dist),
   getVecinosValidos((XP,YP),[(X1,Y1,M,Dist)|L0],L1).
 
-getVecinosValidos((XP,YP),L0,L0).
+getVecinosValidos((_XP,_YP),L0,L0).
 
 
 
 getPersonaCercana((XP,YP),D):-
   t(control(Rol)),
   t(serpiente(Rol,_,[(X,Y)|_],_)),
-  getPersonaCercanaAux((X,Y),[],[(XP,YP,D)|L]).
+  getPersonaCercanaAux((X,Y),[],[(XP,YP,D)|_L]).
 
 getPersonaCercanaAux((XCabeza,YCabeza),L,R):-
   t(cell(X,Y,p)),
+  %nl,write('XP  '),write(X),nl,write('YP  '),write(Y),nl,
   \+in((X,Y,_),L),
   distancia((XCabeza,YCabeza),(X,Y),D),
   head(L,(_,_,D1)),
   D < D1,
-  getPersonaCercanaAux((XCabeza,YCabeza),[(X,Y,D)|L],R),!.
+  getPersonaCercanaAux((XCabeza,YCabeza),[(X,Y,D)|L],R).
 
 getPersonaCercanaAux((XCabeza,YCabeza),[],R):-
   t(cell(X,Y,p)),
   distancia((XCabeza,YCabeza),(X,Y),D),
-  getPersonaCercanaAux((XCabeza,YCabeza),[(X,Y,D)],R).
+  getPersonaCercanaAux((XCabeza,YCabeza),[(X,Y,D)],R),nl.
+  %write('X  '),write(X),nl,write('Y  '),write(Y).
 
-getPersonaCercanaAux((X,Y),L,L).
+getPersonaCercanaAux((_X,_Y),L,L).
 
 
 
 
 %Obtiene de una lista, la mejor acción.
-getMejorAccion([(X,Y,Accion,0)|R],_,MenorDistanciaParcial,Accion,MenorDistancia).
+getMejorAccion([(_X,_Y,Accion,0)|_R],_,_MenorDistanciaParcial,Accion,_MenorDistancia).
 
 getMejorAccion([(_,_,Accion,Distancia)|R],_,MenorDistanciaParcial,MejorAccion,MenorDistancia):-
   Distancia=<MenorDistanciaParcial,
